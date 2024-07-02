d0b04aefe055fc8ca34aaf48425b69f0be5fe0c0
Sascha Nemeth <sascha.nemeth@me.com>
2024-07-02 20:33:57 +0200

    chore: Update Angular core to version 18.0.5 and add CLI analytics tracking

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: "app-login",
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: "./login.component.html",
+    styleUrl: "./login.component.scss",
+})
+export class LoginComponent {
+    email: string = "";
+    password: string = "";
+
+    login() {
+        console.log(this.email, this.password);
+    }
+}
